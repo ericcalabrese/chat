e389904d506f5e0262e6486bdd3c16c9aec8d076
Eric Calabrese <calabrese.eric@gmail.com>
2017-06-23 14:43:31 -0400

    styling done

---
+++ public/js/chat.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+	$("chatWindow").animate({ scrollTop: $('#chatWindow').height()} );
+
+	// $("#chatWindow").scrollTop($('#chatWindow').height());
+
+	console.log("eric");
+});
+
